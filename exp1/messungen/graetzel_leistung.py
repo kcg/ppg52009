618e75c35d0dc1662f541ef749eab95fc1a807e7
pasti4pp <basti.87@gmx.de>
2009-11-03 00:01:00 +0100

    Schaltkreis zur Leistungsmessung mit xcircuit

--- exp1/messungen/graetzel_leistung.py
+++ exp1/messungen/graetzel_leistung.py
@@ -13,8 +13,9 @@ colsep = "\t"
 
 # Die Modellfunktion
 def fitfunc(p, xx):
-	# U² * (R+R1) / (R+R1 + R2)²
-	return [p[0]**2 * (x+p[2]) / (x+p[2]+p[1])**2 for x in xx]
+	# U² * R / (R + R2)²
+	return [p[0]**2 * x / (x + p[1])**2 for x in xx]
+
 
 # Abstandsfunktion zwischen Modell und Daten
 # Muss nur modifiziert werden, wenn unterschiedliche Einzelfehler vorliegen
@@ -40,28 +41,34 @@ for linetext in ifile.readlines():
 			print i, "is not a float"
 	data.append(row)
 
+
+# zusätzlicher Widerstand des Amperemeters
+R_amp = 5.22
 # Werte Spaltenweise
-R1 = [i[0] for i in data]
+R1 = [i[0] + R_amp for i in data]
 U = [i[1] for i in data]
 I = [i[2] for i in data]
 P = [i[1] * i[2] for i in data]
 
 U_error = 0.2
 I_error = 0.1
+# Fehlerfortpflanzung
 P_error = [P[i] * sqrt((U_error/U[i])**2 + (I_error/I[i])**2) for i in range(len(R1))]
 
 # Anfangswert für Parameter
-p0 = [150., 19., 6.]
+p0 = [150., 19.]
 
 # Fit durchführen
 p1, success = optimize.leastsq(errfunc, p0[:], args=(R1, P, P_error))
 print p1
 
+print "fitfunc([0,1,2,3]) =", fitfunc(p1, [0,1,2,3])
+
 xarray = pylab.linspace(R1[0]-2., R1[-1]+2., 100)
 
-pylab.plot(xarray, fitfunc(p1, xarray), "k-", label='$(%.0f\,\mathrm{mV})^2 \cdot \\frac{R+%.1f\,\mathrm{k\Omega}}{(R+%.1f\,\mathrm{k\Omega}+%.1f\,\mathrm{k\Omega})^2}$' % (p1[0], p1[2], p1[2], p1[1]))
+pylab.plot(xarray, fitfunc(p1, xarray), "k-", label='$(%.0f\,\mathrm{mV})^2 \cdot \\frac{R}{(R+%.0f\,\mathrm{k\Omega})^2}$' % (p1[0], p1[1]))
 pylab.errorbar(R1, P, P_error, None, "bo", label="Messwerte")
-pylab.xlim(R1[0] -2.5, R1[-1]+2.5)
+pylab.xlim(0., R1[-1]+3.)
 pylab.ylim(0., 330.)
 
 pylab.xlabel(u"$R\; [\mathrm{k\Omega}]$")
@@ -69,7 +76,7 @@ pylab.ylabel(u"$P\; [\mathrm{nW}]$")
 
 pylab.legend(loc='lower right')
 
-x_max = p1[1] - p1[2]
+x_max = p1[1]
 y_max = p1[0]**2 / (4. * p1[1])
 pylab.annotate('%.0f nW' % (y_max,), xy=(x_max, y_max+18),  xycoords='data', horizontalalignment='center', verticalalignment='bottom')
 
