913269ad45e79b097012e327fcc4e581daa112bb
michele collodo <michele.collodo@online.de>
2010-01-25 17:01:49 +0100

    measure

--- exp3/gui/form_spectral.py
+++ exp3/gui/form_spectral.py
@@ -24,6 +24,7 @@ from matplotlib.figure import Figure
 from spectral_calc import *
 from simulation import *
 from color_spectral import spectral
+import readserial
 
          
 class FormSpectral (threading.Thread, QtGui.QWidget):
@@ -33,6 +34,7 @@ class FormSpectral (threading.Thread, QtGui.QWidget):
 
 		self.pause_continuous = True
 		self.xrange = (345., 675.)
+		self.ser = readserial.ReadSerial()
 		
 		
 		## Fenstereigenschaften:
@@ -171,8 +173,60 @@ class FormSpectral (threading.Thread, QtGui.QWidget):
 		self.draw_initial()
 
 		if self.radio_measure.isChecked():
-			# Testausgabe:
-			self.axes.plot([300,350,400,450,500,550,600,650,700],[random.random() for i in xrange(9)])
+#			# Testausgabe:
+#			self.axes.plot([300,350,400,450,500,550,600,650,700],[random.random() for i in xrange(9)])
+
+			signal = sc.array(self.ser.get_data())
+
+
+			if self.cboxMethod.currentText() == u"pseudo-inverse":
+				y = self.spec.spectrum_pinv(signal)
+				text = u"pseudo-inverse"
+			elif self.cboxMethod.currentText() == u"least-square":
+				y = self.spec.spectrum_leastsqr(signal,
+					self.sliderSmooth.value() / (1. + self.sliderSmooth.maximum()))
+				text = u"least-square"
+			elif self.cboxMethod.currentText() == u"blackbody":
+				T = [0., 0.]
+				y = self.spec.spectrum_blackbody(signal, T)
+				text = u"blackbody $T=%i\,\mathrm{K}$" % (int(T[1]), )
+			elif self.cboxMethod.currentText() == u"polynomial":
+				y = self.spec.spectrum_polynomial(self.simulation.signal)
+				text = "polynomial"
+			elif self.cboxMethod.currentText() == u"spline":
+				y = self.spec.spectrum_spline(signal)
+				text = "spline"
+			elif self.cboxMethod.currentText() == u"exact+smooth":
+				y = self.spec.spectrum_smooth(signal)
+				text = "exact and as smooth as possible"
+			elif self.cboxMethod.currentText() == u"optimize":
+				y = self.spec.spectrum_optimize(signal,
+					self.sliderSmooth.value() / float(self.sliderSmooth.maximum()))
+				text = "nonlinear optimized"
+
+			self.axes.plot(self.spec.lambdas, y, "r-", linewidth=4, label=text)
+			self.axes.set_xlim(self.xrange)
+			self.axes.legend(loc="best")
+			
+			led_label = ["375nm", "395nm", "468nm", "480nm", "503nm", "505nm", "515nm", "525nm", "540nm", "588nm", "588nm", "605nm", "620nm", "628nm", "635nm", "640nm"]
+			x = [i for i in xrange(1, self.spec.n + 1)]
+			y = signal# / self.spec.weights
+			maxi = max(signal)
+			y = y / max(y)
+			colors = []
+			for i in range(len(led_label)):
+				try:
+					j = float(led_label[i][0:3])
+					colors.append(spectral(j))
+				except ValueError:
+					colors.append(spectral(self.spec.led_colors[i]))
+			#colors = [spectral(i) for i in self.spec.led_colors]
+			self.axes2.bar(x, y, width=0.9, color=colors, align="center", label="max: "+str(round(maxi,2)))
+			self.axes2.set_xlim(.4, self.spec.n + .6)
+			self.axes2.set_xticks(range(1, self.spec.n+1), minor=False)
+			self.axes2.set_xticklabels(led_label, fontdict=None, minor=False, rotation=45)
+			self.axes2.legend(loc="best")
+			
 		else:
 			try:
 				self.simulation.make_signal(self.spec.A, self.noise_from_slider())
--- exp3/gui/readserial.py
+++ exp3/gui/readserial.py
@@ -10,38 +10,45 @@ class ReadSerial ():
 	def __init__ (self):
 	
 		self.baudrate		= 9600
-		self.read_delay 	= 0.2
+		self.read_delay 	= 2.0
 		self.adc_upper_limit	= 1000
 		self.adc_lower_limit	= 30
-		self.data_lenght	= 100
+		self.data_lenght	= 200
 		
 		self.ser = serial.Serial(0, self.baudrate, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, \
 			stopbits=serial.STOPBITS_ONE, timeout=1, xonxoff=0, rtscts=0)
 			
 		# Teste Hardware:	
-		self.ser.write("?")
-		time.sleep(0.1)
-		if "spectral" in self.ser.read(10):
+#		self.ser.write("?")
+#		time.sleep(1)
+#		if "spectral" in self.ser.read(10):
 			# Teste korrekte Verstärker-Einstellung:
-			self.ser.write(".")
-			time.sleep(0.1)
-			if int(self.ser.read(10)) > self.adc_upper_limit:
-				print ("Einstellungen für AREF fehlerhaft!")
-		else:
-			print "Hardware nicht angeschlossen!"
-			self.ser.close()
+#			self.ser.write(".")
+#			time.sleep(0.1)
+			#if int(self.ser.read(10)) > self.adc_upper_limit:
+			#	print ("Einstellungen für AREF fehlerhaft!")
+#		else:
+#			print "Hardware nicht angeschlossen!"
+#			self.ser.close()
 
 		
-	def get_data ():
+	def get_data (self):
 		# Gibt Intensitätswerte der Leds zurück
 		# Format:	[I(led0), I(led1), ..., I(led15)]
-		
+		self.data = []
 		self.ser.write("!")
 		time.sleep(self.read_delay)
 		self.data = self.ser.read(self.data_lenght)
-		self.data = self.data.split(".")
-		self.data = [int(self.data[i]) for i in xrange(len(self.data))]
+		self.data = self.data.split(";")
+		data2 = []
+		for i in xrange(len(self.data)):
+			try:
+				data2.append(float(self.data[i]))
+			except ValueError:
+				pass
+		self.data = data2
+#		print self.data
 		return self.data
 		
-	def close_port ():
+	def close_port (self):
 		self.ser.close()		
